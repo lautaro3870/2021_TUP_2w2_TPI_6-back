26966902bf2b52c1f4c8d1374f605d782701e0ee
lautaro disbroiavacca <111992@tecnicatura.frc.utn.edu.ar>
2021-10-23 18:16:10 -0300

    Post y dar de baja Productos

--- back_MSI_SuperMami/back_MSI_SuperMami/Controllers/ProductoController.cs
+++ back_MSI_SuperMami/back_MSI_SuperMami/Controllers/ProductoController.cs
@@ -1,4 +1,5 @@
-﻿using back_MSI_SuperMami.Models;
+﻿using back_MSI_SuperMami.Comandos;
+using back_MSI_SuperMami.Models;
 using back_MSI_SuperMami.Respuestas;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
@@ -22,19 +23,139 @@ namespace back_MSI_SuperMami.Controllers
             _logger = logger;
         }
 
+        //Productos habilitados
         [HttpGet]
         [Route("[controller]/Get")]
         public ActionResult<RespuestaAPI> Get()
         {
             var respuestas = new RespuestaAPI();
             respuestas.Ok = true;
-            respuestas.Respuesta = bd.Productos.Where(x => x.Estado == true).ToList();           
+            respuestas.Respuesta = bd.Productos.Where(x => x.Estado == true).ToList();
             return respuestas;
         }
 
-        
+        //Dar de baja producto
+        [HttpPut]
+        [Route("[controller]/DarDeBaja")]
+        public ActionResult<RespuestaAPI> DarDeBaja(int id)
+        {
+            var res = new RespuestaAPI();
+            
+
+            if (id == 0)
+            {
+                res.Ok = false;
+                res.Respuesta = "Producto no encontrado";
+                return res;
+            }
+            else
+            {
+                try
+                {
+                    var pro = bd.Productos.Where(x => x.Idproducto == id).FirstOrDefault();
+                    if (pro != null && pro.Estado == true)
+                    {
+                        pro.Estado = false;
+                        res.Ok = true;
+                        res.Respuesta = pro;
+                        res.Error = "Producto dado de baja";
+                        bd.Productos.Update(pro);
+                        bd.SaveChanges();
+
+                    }
+                    else
+                    {
+                        res.Ok = false;
+                        res.Respuesta = "Producto no encontrado";
+                        return res;
+                    }
+                    return res;
+
+                }
+                catch (Exception e)
+                {
+                    res.Ok = false;
+                    res.Error = "No hay Productos habilitados";
+                    return res;
+                }
+            }
+        }
+
+        //Insertar producto
+        [HttpPost]
+        [Route("[controller]/Post")]
+        public RespuestaAPI PostProducto([FromBody] ComandoRegistrarProducto comando)
+        {
+            RespuestaAPI res = new RespuestaAPI();
+
+            if (string.IsNullOrEmpty(comando.Nombre))
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso el nombre";
+                return res;
+            }
+            if (string.IsNullOrEmpty(comando.Descripcion))
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso la descripción";
+                return res;
+            }
+
+            if (comando.Precio == 0)
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso el precio";
+                return res;
+            }
+            if (comando.Vencimiento.Equals(""))
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso la fecha de vencimiento";
+                return res;
+            }
+            
+            if (comando.unidadMedida == 0)
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso la unidad de medida";
+                return res;
+            }
+
+            if (comando.Categoria == 0)
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso la categoria";
+                return res;
+            }
+            if (comando.Marca == 0)
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso la marca";
+                return res;
+            }
+
+            Producto p = new Producto();
 
 
+            p.Nombre = comando.Nombre;
+            p.Descripcion = comando.Descripcion;
+            p.Precio = comando.Precio;
+            p.Vencimiento = comando.Vencimiento;
+            p.Estado = true;
+            p.Idunidadmedida = comando.unidadMedida;
+            p.Idcategoria = comando.Categoria;
+            p.Idmarca = comando.Marca;
+            
+            bd.Productos.Add(p);
+            bd.SaveChanges();
+
+            res.Ok = true;
+            res.InfoAdicional = "Producto insertado correctamente";
+            return res;
+
+        }
 
     }
+
+    
 }
