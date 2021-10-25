b473326ba4b73c7fcbb5a597ddfa344cac37635f
Facundo Moyano <112015@tecnicatura.frc.utn.edu.ar>
2021-10-25 17:28:13 -0300

    Login response

---
+++ back_MSI_SuperMami/back_MSI_SuperMami/Helpers/HashHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_MSI_SuperMami.Helpers
+{
+    public class HashHelper
+    {
+
+        public static HashedPassword Hash(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            return new HashedPassword() { Password = hashed, Salt = Convert.ToBase64String(salt) };
+        }
+
+        
+        public class HashedPassword
+        {
+            public string Password { get; set; }
+            public string Salt { get; set; }
+        }
+    }
+    
+}
+
+
+
+
---
+++ back_MSI_SuperMami/back_MSI_SuperMami/Models/login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_MSI_SuperMami.Models
+{
+    public class login
+    {
+        public login(string mail, string token, string rol)
+        {
+            this.Mail = mail;
+            this.Token = token;
+            this.Rol = rol;
+        }
+
+        private string mail;
+        private string token;
+        private string rol;
+        
+        public string Mail { get => mail; set => mail = value; }
+        public string Token { get => token; set => token = value; }
+        public string Rol { get => rol; set => rol = value; }
+    }
+}
