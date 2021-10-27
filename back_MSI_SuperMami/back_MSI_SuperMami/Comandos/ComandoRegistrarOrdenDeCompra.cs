80a6aeaa110c0858f24f77ed78ae47980d29a985
lautaro disbroiavacca <111992@tecnicatura.frc.utn.edu.ar>
2021-10-27 15:12:39 -0300

    Get, delete y post de ordenes de compra

---
+++ back_MSI_SuperMami/back_MSI_SuperMami/Comandos/ComandoRegistrarOrdenDeCompra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_MSI_SuperMami.Comandos
+{
+    public class ComandoRegistrarOrdenDeCompra
+    {
+        public int proveedor { get; set; }
+        public int formadeenvio { get; set; }
+        public int formadepago { get; set; }
+    }
+}
---
+++ back_MSI_SuperMami/back_MSI_SuperMami/Controllers/OrdenDeCompraController.cs
@@ -0,0 +1,162 @@
+﻿using back_MSI_SuperMami.Comandos;
+using back_MSI_SuperMami.Models;
+using back_MSI_SuperMami.Respuestas;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_MSI_SuperMami.Controllers
+{
+    [ApiController]
+    [EnableCors("MSI2021")]
+    public class OrdenDeCompraController
+    {
+
+        private readonly d4nfd5l4d933b1Context bd = new d4nfd5l4d933b1Context();
+        private readonly ILogger<OrdenDeCompraController> _logger;
+
+        public OrdenDeCompraController(ILogger<OrdenDeCompraController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("ordenesdecompra")]
+        public ActionResult<RespuestaAPI> Get()
+        {
+            var respuesta = new RespuestaAPI();
+            respuesta.Ok = true;
+            respuesta.Respuesta = bd.OrdenesDeCompras.Where(x => x.Idestado == 3).ToList();
+            return respuesta;
+        }
+
+        [HttpGet]
+        [Route("ordenesdecompra/{id}")]
+        public ActionResult<RespuestaAPI> GetPorId(int id)
+        {
+            var respuesta = new RespuestaAPI();
+            if (id.Equals(0))
+            {
+                respuesta.Ok = false;
+                respuesta.Respuesta = "Ingrese una orden de compra";
+                return respuesta;
+            }
+            else
+            {
+                var orden = bd.OrdenesDeCompras.Find(id);
+                try
+                {
+                    if (orden != null)
+                    {
+                        respuesta.Ok = true;
+                        respuesta.Respuesta = orden;
+                        return respuesta;
+                    }
+                    return respuesta;
+                }
+                catch
+                {
+                    respuesta.Ok = false;
+                    respuesta.Respuesta = "No se encuentra la orden de compra solicitada";
+                    return respuesta;
+                }
+            }
+        }
+
+        [HttpDelete]
+        [Route("ordenesdecompra/{id}")]
+        public ActionResult<RespuestaAPI> Delete(int id)
+        {
+            var res = new RespuestaAPI();
+
+            if (id == 0)
+            {
+                res.Ok = false;
+                res.Respuesta = "Orden de compra no encontrado";
+                return res;
+            }
+            else
+            {
+                try
+                {
+                    var orden = bd.OrdenesDeCompras.Where(x => x.Idordendecompra == id).FirstOrDefault();
+                    if (orden != null && orden.Idestado == 3)
+                    {
+                        orden.Idestado = 2;
+                        res.Ok = true;
+                        res.Respuesta = orden;
+                        res.Error = "Orden de compra dada de baja";
+                        bd.OrdenesDeCompras.Update(orden);
+                        bd.SaveChanges();
+
+                    }
+                    else
+                    {
+                        res.Ok = false;
+                        res.Respuesta = "Orden de compra no encontrado";
+                        return res;
+                    }
+                    return res;
+
+                }
+                catch (Exception e)
+                {
+                    res.Ok = false;
+                    res.Error = "No hay Ordenes de compra habilitadas";
+                    return res;
+                }
+            }
+        }
+
+
+        [HttpPost]
+        [Route("ordenesdecompra")]
+        public RespuestaAPI PostOrden([FromBody] ComandoRegistrarOrdenDeCompra comando)
+        {
+            RespuestaAPI res = new RespuestaAPI();
+
+            if (comando.proveedor == 0)
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso el proveedor";
+                return res;
+            }
+            if (comando.formadeenvio == 0)
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso la forma de envio";
+                return res;
+            }
+
+            if (comando.formadepago == 0)
+            {
+                res.Ok = false;
+                res.Error = "No se ingreso la forma de pago";
+                return res;
+            }
+
+            OrdenesDeCompra orden = new OrdenesDeCompra();
+            orden.Idproveedor = comando.proveedor;
+            orden.Idformadeenvio = comando.formadeenvio;
+            orden.Idformapago = comando.formadepago;
+            orden.Idestado = 1;
+
+            bd.OrdenesDeCompras.Add(orden);
+            bd.SaveChanges();
+
+            res.Ok = true;
+            res.InfoAdicional = "Orden de compra insertada correctamente";
+            return res;
+
+        }
+
+    }
+
+
+
+}
+
