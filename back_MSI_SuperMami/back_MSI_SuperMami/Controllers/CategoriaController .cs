3c27c7844c0eec2bfe22b714f0648e1aaff141c9
LordReactX <byboygringop@gmail.com>
2021-10-26 21:53:03 -0300

    saco cors

--- back_MSI_SuperMami/back_MSI_SuperMami/Controllers/CategoriaController .cs
+++ back_MSI_SuperMami/back_MSI_SuperMami/Controllers/CategoriaController .cs
@@ -13,7 +13,6 @@ using System.Threading.Tasks;
 namespace back_MSI_SuperMami.Controllers
 {
     [ApiController]
-    [EnableCors("MSI2021")]
     public class CategoriaController : ControllerBase
     {
 
