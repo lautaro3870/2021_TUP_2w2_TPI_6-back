401d8e98fd55378c5ebb75b5ea07ab02bd571284
lautaro disbroiavacca <111992@tecnicatura.frc.utn.edu.ar>
2021-11-09 17:25:08 -0300

    Get ordenes de compra

---
+++ back_MSI_SuperMami/back_MSI_SuperMami/DTOs/DTOOrdenDeCompraListado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_MSI_SuperMami.DTOs
+{
+    public class DTOOrdenDeCompraListado
+    {
+        public string proveedor { get; set; }
+        public string formaPago { get; set; }
+        public string formaEnvio { get; set; }
+        public string producto { get; set; }
+        public int cantidad { get; set; }
+    }
+}
