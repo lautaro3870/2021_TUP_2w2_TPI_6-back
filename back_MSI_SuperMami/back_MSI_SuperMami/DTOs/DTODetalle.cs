f1a0d1f29e31debbc9fcb6cad4e6266c50b5ab44
2W2112061PaguagaSantiago <112061@tecnicatura.frc.utn.edu.ar>
2021-11-17 13:28:01 -0300

    Agregado campo nombre en Detalle metodo get ordenes de compra

--- back_MSI_SuperMami/back_MSI_SuperMami/DTOs/DTODetalle.cs
+++ back_MSI_SuperMami/back_MSI_SuperMami/DTOs/DTODetalle.cs
@@ -10,7 +10,7 @@ namespace back_MSI_SuperMami.DTOs
 
         public int idproducto { get; set; }
 
-        public String nombre;
+        public String nombre { get; set; }
         //public string nombre { get; set; }
         //public string descripcion { get; set; }
         //public string marca { get; set; }
